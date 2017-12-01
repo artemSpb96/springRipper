c41d1794c32287c37f4518ca6a0add0ea285b0e1
Artem Ananchenko <artem.ananchenko@yandex.ru>
2017-12-01 19:21:46 +0300

    Add InjectRandomInt annotation

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -3,11 +3,17 @@ package quoters;
 public class TerminatorQuoter implements Quoter {
     private String message;
 
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     public void setMessage(String message) {
         this.message = message;
     }
 
     public void sayQuote() {
-        System.out.println("Message = " + message);
+
+        for (int i = 0; i < repeat; ++i) {
+            System.out.println("Message = " + message);
+        }
     }
 }
